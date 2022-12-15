f392fa2a04acfd019f35c0fffb39905c38ffbb44
hnhtran <tran_eetas@yahoo.com>
2022-12-16 09:48:21 +1100

    install react-icons and Sidebar init

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div>
+        <img src="tranGH.jpeg" alt="profile picture" />
+        <h3>Tran Huynh</h3>
+        <p>Software Engineer</p>
+        <p>Download Resume</p>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Sidebar } from '../components/Sidebar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className='grid grid-cols-12 gap-6  px-5 lg:px-48 my-14'>
+    <div className='lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center'>
+      <Sidebar />
+    </div>
+    <div className='lg:col-span-9 bg-white rounded-2xl col-span-12'>    
+    <Component {...pageProps} />
+    </div>
+  </div>
 }
