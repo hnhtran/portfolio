caf4ead740cfab09efa014325d3fb039c90e1e95
hnhtran <tran_eetas@yahoo.com>
2023-01-04 23:15:01 +1100

    framer-motion for projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,7 @@ import { projects as projectsData } from "../data";
 import ProjectCard from "../components/ProjectCard";
 import ProjectsNavBar from "../components/ProjectsNavBar";
 import { Category } from "../type";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   const [projects, setProjects] = useState(projectsData);
@@ -21,6 +22,25 @@ export default function Projects() {
     setActive(category);
   };
 
+  const variants = {
+    initial: {
+      opacity: 0,
+      y: 20,
+    },
+    animate: {
+      opacity: 1,
+      y: 0
+    },
+  };
+  const stagger = {
+    initial: {},
+    animate: {
+      transition: {
+        staggerChildren: 0.15,
+      }
+    }
+  }
+
   return (
     <div className="px-5 py-2" style={{ height: "75vh" }}>
       <ProjectsNavBar
@@ -28,13 +48,19 @@ export default function Projects() {
         active={active}
       />
       <div className="overflow-y-auto" style={{height: "60vh"}}>
-        <div className="relative grid grid-cols-12 gap-4 m-5 w-fit">
+        <motion.div 
+        variants={stagger}
+        initial="initial"
+        animate="animate"
+        className="relative grid grid-cols-12 gap-4 m-5 w-fit">
         {projects.map((project) => (
-          <div key={project.name} className="col-span-12 p-2 m-2 bg-gray-200 rounded-lg dark:bg-dark-200 lg:col-span-4" id="col-span-6-modified">
+          <motion.div 
+          variants={variants}
+          key={project.name} className="col-span-12 p-2 m-2 bg-gray-200 rounded-lg dark:bg-dark-200 lg:col-span-4" id="col-span-6-modified">
             <ProjectCard project={project} key={project.name} />
-          </div>
+          </motion.div>
         ))}
-        </div>
+        </motion.div>
       </div>
     </div>
   );
